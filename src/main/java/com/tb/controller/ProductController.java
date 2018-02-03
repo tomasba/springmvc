e78d78fa543901bba0dddbfb9991bf39241a65ec
Tomas <tomas.baltrukonis@gmail.com>
2018-02-03 16:30:50 +0200

    add product controller mvc unittest

--- src/main/java/com/tb/controller/ProductController.java
+++ src/main/java/com/tb/controller/ProductController.java
@@ -11,41 +11,42 @@ import com.tb.domain.Product;
 import com.tb.service.ManagementService;
 
 @Controller
+@RequestMapping(path="/products")
 public class ProductController {
 	
 	private ManagementService<Product> productService;	
 
-	@RequestMapping(method=RequestMethod.GET, path="/products")
+	@RequestMapping(method=RequestMethod.GET)
 	public String findAllProducts(Model model) {
 		model.addAttribute("products", productService.findAll());
 		return "products";
 	}
 	
-	@RequestMapping(method=RequestMethod.GET, path="/products/{id}")
+	@RequestMapping(method=RequestMethod.GET, path="/{id}")
 	public String findProduct(@PathVariable Integer id, Model model) {
 		model.addAttribute("product", productService.find(id));
 		return "product";
 	}
 	
-	@RequestMapping(method=RequestMethod.GET, path="/products/new")
+	@RequestMapping(method=RequestMethod.GET, path="/new")
 	public String createProduct(Model model) {
 		model.addAttribute("product", new Product());
 		return "productform";
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, path="/products")
+	@RequestMapping(method=RequestMethod.POST)
 	public String saveOrUpdateProduct(Product product) {
 		Product saved = productService.saveOrUpdate(product);
 		return "redirect:/products/" + saved.getId();
 	}	
 	
-	@RequestMapping(path="/products/update/{id}")
+	@RequestMapping(path="/update/{id}")
 	public String editProduct(@PathVariable Integer id, Model model) {		
 		model.addAttribute("product", productService.find(id));
 		return "productform";
 	}
 	
-	@RequestMapping(path="/products/delete/{id}")
+	@RequestMapping(path="/delete/{id}")
 	public String deleteProduct(@PathVariable Integer id) {
 		productService.delete(id);
 		return "redirect:/products";
---
+++ src/test/java/com/tb/controller/ProductControllerTest.java
@@ -0,0 +1,143 @@
+package com.tb.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.tb.domain.Product;
+import com.tb.service.ManagementService;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by jt on 11/16/15.
+ */
+public class ProductControllerTest {
+
+	@Mock
+	private ManagementService<Product> productService;
+
+	@InjectMocks
+	private ProductController productController;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this); // initilizes controller and mocks
+
+		mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+	}
+
+	@Test
+	public void testList() throws Exception {
+		// given
+		List<Product> products = new ArrayList<>();
+		products.add(new Product());
+		products.add(new Product());
+		when(productService.findAll()).thenReturn((List<Product>) products);
+
+		// when
+		ResultActions result = mockMvc.perform(get("/products/"));
+
+		// then
+		result.andExpect(status().isOk()).andExpect(view().name("products"))
+				.andExpect(model().attribute("products", hasSize(2)));
+	}
+
+	@Test
+	public void testShow() throws Exception {
+		// given
+		Integer id = 1;
+		when(productService.find(id)).thenReturn(new Product());
+
+		// when
+		ResultActions result = mockMvc.perform(get("/products/1"));
+
+		// then
+		result.andExpect(status().isOk()).andExpect(view().name("product"))
+				.andExpect(model().attribute("product", instanceOf(Product.class)));
+	}
+
+	@Test
+	public void testEdit() throws Exception {
+		// given
+		Integer id = 1;
+		when(productService.find(id)).thenReturn(new Product());
+		// when
+		ResultActions result = mockMvc.perform(get("/products/update/1"));
+		// then
+		result.andExpect(status().isOk()).andExpect(view().name("productform"))
+				.andExpect(model().attribute("product", instanceOf(Product.class)));
+	}
+
+	@Test
+	public void testNewProduct() throws Exception {
+		// given
+		Integer id = 1;
+		// should not call service
+		verifyZeroInteractions(productService);
+		// when
+		ResultActions result = mockMvc.perform(get("/products/new"));
+		// then
+		result.andExpect(status().isOk()).andExpect(view().name("productform"))
+				.andExpect(model().attribute("product", instanceOf(Product.class)));
+	}
+
+	@Test
+	public void testSaveOrUpdate() throws Exception {
+		Integer id = 1;
+		String description = "Test Description";
+		BigDecimal price = new BigDecimal("12.00");
+		String imageUrl = "example.com";
+
+		Product returnProduct = new Product();
+		returnProduct.setId(id);
+		returnProduct.setDescription(description);
+		returnProduct.setPrice(price);
+		returnProduct.setImageUrl(imageUrl);
+
+		when(productService.saveOrUpdate(Matchers.<Product>any())).thenReturn(returnProduct);
+
+		mockMvc.perform(post("/products").param("id", "1").param("description", description).param("price", "12.00")
+				.param("imageUrl", "example.com")).andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/products/1"))
+				.andExpect(model().attribute("product", instanceOf(Product.class)))
+				.andExpect(model().attribute("product", hasProperty("id", is(id))))
+				.andExpect(model().attribute("product", hasProperty("description", is(description))))
+				.andExpect(model().attribute("product", hasProperty("price", is(price))))
+				.andExpect(model().attribute("product", hasProperty("imageUrl", is(imageUrl))));
+
+		// verify properties of bound object
+		ArgumentCaptor<Product> boundProduct = ArgumentCaptor.forClass(Product.class);
+		verify(productService).saveOrUpdate(boundProduct.capture());
+
+		assertEquals(id, boundProduct.getValue().getId());
+		assertEquals(description, boundProduct.getValue().getDescription());
+		assertEquals(price, boundProduct.getValue().getPrice());
+		assertEquals(imageUrl, boundProduct.getValue().getImageUrl());
+	}
+	
+	 @Test
+	 public void testDelete() throws Exception{
+	 Integer id = 1;
+	
+	 mockMvc.perform(get("/products/delete/1"))
+	 .andExpect(status().is3xxRedirection())
+	 .andExpect(view().name("redirect:/products"));
+	
+	 verify(productService, times(1)).delete(id);
+	 }
+}
